127e275f36b5fa01a1e29c55a78fea56c892b5fc
郑祯睿 <ch4acko3@outlook.com>
2025-02-16 05:13:33 +0800

    Bug fixes for python package init file

--- python/aerodrome/__init__.py
+++ python/aerodrome/__init__.py
@@ -2,7 +2,6 @@ import importlib
 import os
 
 from aerodrome.core import *
-# from aerodrome.envs.c_envs import BaseEnv
 
 # C++ envs
 envs_dir = os.path.join(os.path.dirname(__file__), "simulator")
@@ -10,7 +9,8 @@ envs_dir = os.path.join(os.path.dirname(__file__), "simulator")
 for root, dirs, files in os.walk(envs_dir):
     for file in files:
         if file.endswith(".pyd"):
-            relative_path = os.path.relpath(os.path.join(root, file), os.path.dirname(__file__))[:-4].replace('\\', '.')
+            relative_path = os.path.relpath(os.path.join(root, file), os.path.dirname(__file__))[:-4].replace('\\', '.').split('.')[:-1]
+            relative_path = '.'.join(relative_path)
             module_name = f"aerodrome.{relative_path}"
             importlib.import_module(module_name)
 
